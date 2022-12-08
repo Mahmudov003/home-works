b2b5b01d37bc2ba2eb20e5a9a2e4908eddfca43d
Asliddin <asliddinmakhmudov003@mail.com>
2022-12-08 23:48:24 +0500

    return

--- nt-1/script.js
+++ nt-1/script.js
@@ -1,10 +1,11 @@
 "use strict";
 
-let notice1 = document.getElementsByClassName('n1');
-let notice2 = document.getElementsByClassName('n2');
-let input_1 = document.getElementById("input_1");
 
 input_1.addEventListener("input", () => {
+  let notice1 = document.getElementsByClassName('n1')[0];
+  let notice2 = document.getElementsByClassName('n2')[0];
+  let input_1 = document.getElementById("input_1");
+
   let lorem = document.getElementById("lorem");
   let param = document.getElementsByClassName("param")[0];
 
@@ -12,23 +13,23 @@ input_1.addEventListener("input", () => {
 
   if (result === true) {
     // notice1.style.display = "block";
-    input_1.style.background = "green";
-    param.innerHTML = "Ushbu so'z matnda mavjud!";
+    // input_1.style.background = "green";
+    // param.innerHTML = "Ushbu so'z matnda mavjud!";
   } else if (result === false) {
     // notice2.style.display = "block";
-    input_1.style.background = "red";
-    param.innerHTML = "Ushbu so'z matnda mavjud emas!";
+    // input_1.style.background = "red";
+    // param.innerHTML = "Ushbu so'z matnda mavjud emas!";
   } else {
-    param.innerHTML = "Maydon bo'sh!!!";
+    alert ("Maydon bo'sh!!!");
   }
 });
 //////////////////////////////////////////////
 
 let meva = document.getElementById("meva");
 let input_2 = document.getElementById("input_2");
-let params = document.getElementById("params")[0];
+let params = document.getElementById("params");
 
-let btn = document.getElementsByClassName("btn");
+let btn = document.getElementsByClassName("btn")[0];
 
 btn.addEventListener("click", () => {
   let results = meva.innerText.includes(input_2.value);
@@ -41,5 +42,12 @@ btn.addEventListener("click", () => {
   }
 });
 
+// //////////////////////////////////////////////////
+let form = document.getElementById('form');
+
+        form.onsubmit = function (e) {
+            e.preventDefault()[0];
+       
+        };
 
 
